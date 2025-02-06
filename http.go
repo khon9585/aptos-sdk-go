f4a479680c3f23835768d59640cfd2a5d6cc26d6
khon9585 <ggtrhgtrq3eb4@hotmail.com>
2025-02-07 00:40:08 +0900

    code

---
+++ http.go
@@ -0,0 +1,123 @@
+package aptos
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+)
+
+const (
+	maxRequestContentLength = 1024 * 1024 * 5
+	contentType             = "application/json"
+)
+
+// HTTPError is a wrap for HTTP's not 2xx codes
+type HTTPError struct {
+	StatusCode int
+	Status     string
+	Body       []byte
+}
+
+func (err HTTPError) Error() string {
+	if len(err.Body) == 0 {
+		return err.Status
+	}
+	return fmt.Sprintf("%v: %s", err.Status, err.Body)
+}
+
+type httpConn struct {
+	client    *http.Client
+	url       string
+	closeOnce sync.Once
+	closeCh   chan interface{}
+	mu        sync.Mutex
+	headers   http.Header
+}
+
+func newHTTPConn(endpoint string) *httpConn {
+	conn := &httpConn{}
+	conn.client = new(http.Client)
+	conn.headers = make(http.Header, 2)
+	conn.headers.Set("accept", contentType)
+	conn.headers.Set("content-type", contentType)
+	conn.url = endpoint
+	conn.closeCh = make(chan interface{})
+
+	return conn
+}
+
+func (hc *httpConn) postJSON(ctx context.Context, urlpath string, msg interface{}) (io.ReadCloser, error) {
+	endpoint := fmt.Sprintf("%s/%s", hc.url, urlpath)
+	body, err := json.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", endpoint, io.NopCloser(bytes.NewReader(body)))
+	if err != nil {
+		return nil, err
+	}
+	req.ContentLength = int64(len(body))
+	req.GetBody = func() (io.ReadCloser, error) { return io.NopCloser(bytes.NewReader(body)), nil }
+
+	// set headers
+	hc.mu.Lock()
+	req.Header = hc.headers.Clone()
+	hc.mu.Unlock()
+
+	// do request
+	resp, err := hc.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		var buf bytes.Buffer
+		var body []byte
+		if _, err := buf.ReadFrom(resp.Body); err == nil {
+			body = buf.Bytes()
+		}
+
+		return nil, HTTPError{
+			Status:     resp.Status,
+			StatusCode: resp.StatusCode,
+			Body:       body,
+		}
+	}
+	return resp.Body, nil
+}
+
+func (hc *httpConn) get(ctx context.Context, urlpath string, msg interface{}) (io.ReadCloser, error) {
+	rpath := endodeURLPath(urlpath, msg)
+	endpoint := fmt.Sprintf("%s/%s", hc.url, rpath)
+	req, err := http.NewRequestWithContext(ctx, "GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// set headers
+	hc.mu.Lock()
+	req.Header = hc.headers.Clone()
+	hc.mu.Unlock()
+
+	// do request
+	resp, err := hc.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		var buf bytes.Buffer
+		var body []byte
+		if _, err := buf.ReadFrom(resp.Body); err == nil {
+			body = buf.Bytes()
+		}
+		return nil, HTTPError{
+			Status:     resp.Status,
+			StatusCode: resp.StatusCode,
+			Body:       body,
+		}
+	}
+	return resp.Body, nil
+}
